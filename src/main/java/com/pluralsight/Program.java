6661da5feee8c4299bd00b9437c9bab7b1c235e3
mili Arsema <meleymm88@gmail.com>
2026-04-21 12:00:43 -0500

    Added user input for choice of story

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,45 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
+    static Scanner keyboard = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //user chooses a story
+        System.out.println("Stories available: goldilocks.txt" +
+                "\nhansel_and_gretel.txt" +
+                "\nmary_had_a_little_lamb.txt");
+        System.out.print("Enter the name of the story: ");
+        String fileName = keyboard.nextLine();
+
+        //pull story file
+        FileReader fileReader = new FileReader(fileName);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        //connect user choice w file reader
+
+
+        String storyLine = bufferedReader.readLine();
+        //int for numbering system of lines
+        int number = 1;
+
+        //print chosen file
+        while (storyLine != null) {
+            //TODO: add sout for numbering before printing line
+            /* numbering system~ printed lines
+             * ++number: 1 + number vs number++: number + 1
+             */
+            System.out.print(number++ + ". ");
+            System.out.println(storyLine);
+            storyLine = bufferedReader.readLine();
+        }
+
+
+    }
+
+    //story file-reader methods
+
 }
